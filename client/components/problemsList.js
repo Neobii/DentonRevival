4cc4bbe6246f799fb3f61edd8eecd0307da0fe8f
root <root@localhost.localdomain>
2016-06-07 07:50:46 -0400

    DentonRevial

--- client/components/problemsList.js
+++ client/components/problemsList.js
@@ -1,3 +1,11 @@
+Template.problemsList.onCreated(function(){
+	var instance = this;
+	instance.autorun(function(){
+		instance.subscribe('problems');
+	});
+});
+
+
 Template.problemsList.helpers({
 	problems() {
 		return Problems.find({}, {sort: {score: -1}})
